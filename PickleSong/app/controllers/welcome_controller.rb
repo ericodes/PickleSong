b9d8e8439d44ec631938c3a4ec06d3e6c102479e
ericodes <erica.cheung@flatironschool.com>
2014-07-22 17:41:19 -0400

    Search works and video result appear

--- PickleSong/app/controllers/welcome_controller.rb
+++ PickleSong/app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     results = SongPicklr::Search.new(params[:q])
-    @result = results.query_items.first
+    @result = results.query_items.first if params[:q]
     render :index
   end
 end
