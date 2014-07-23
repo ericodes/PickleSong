3d18c9f17b8268dbb83f55c003f3f1321f21f812
Sam Blake <samuelmorganblake@gmail.com>
2014-07-23 17:09:01 -0400

    working on index

--- PickleSong/app/assets/javascripts/welcome.js
+++ PickleSong/app/assets/javascripts/welcome.js
@@ -3,6 +3,8 @@ $(document).on('page:change', function() {
         var songData = $('.playlist').data('songs');
         for (var i = 0; i < songData.length; i++) {
             $('#playlist_songs').append("<div class=song><h4>" + songData[i].title + "</h4>" + "<div><img src=" + songData[i].thumbnail + " /></div></div>");
-        };
+        }
     });
 });
+
+
