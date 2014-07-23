c5921fb393f480ea017f916577658c776175ad16
emilysimonton <emily.simonton@gmail.com>
2014-07-23 14:31:53 -0400

    playlist song divs

--- PickleSong/app/assets/javascripts/welcome.js
+++ PickleSong/app/assets/javascripts/welcome.js
@@ -1,8 +1,8 @@
-$( document ).on( 'page:change', function () {
-  $('.playlist').click(function() {
-    var songData = $('.playlist').data('songs');
-    for(var i=0; i<songData.length; i++) {
-      $('#playlist_songs').append(songData[i].name);
-    };
-  });
+$(document).on('page:change', function() {
+    $('.playlist').click(function() {
+        var songData = $('.playlist').data('songs');
+        for (var i = 0; i < songData.length; i++) {
+            $('#playlist_songs').append("<div class=song><h4>" + songData[i].title + "</h4>" + "<div><img src=" + songData[i].thumbnail + " /></div></div>");
+        };
+    });
 });
