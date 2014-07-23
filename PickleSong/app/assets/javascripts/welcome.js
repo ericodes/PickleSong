f71ada616c8e9fae29d5b0742c311b4d37ce7e2a
ericodes <erica.cheung@flatironschool.com>
2014-07-23 19:03:39 -0400

    Refactored JS on results

--- PickleSong/app/assets/javascripts/welcome.js
+++ PickleSong/app/assets/javascripts/welcome.js
@@ -9,29 +9,8 @@ $(document).on('page:change', function() {
 
 
 $(document).ready(function() {
-    $('.vid-item-0').click(function() {
-        var embedUrl = $('.vid-item-0').data('embed_url');
-        // console.log(embedUrl);
-        // console.log("What's in iframe now: " + document.getElementById('vid_frame').src);
-        document.getElementById('vid_frame').src = "//" + embedUrl;
-        // console.log("Replaced: " + document.getElementById('vid_frame').src);
-    });
-    $('.vid-item-1').click(function() {
-        var embedUrl = $('.vid-item-1').data('embed_url');
-        // console.log(embedUrl);
-        // console.log("What's in iframe now: " + document.getElementById('vid_frame').src);
-        document.getElementById('vid_frame').src = "//" + embedUrl;
-        // console.log("Replaced: " + document.getElementById('vid_frame').src);
-    });
-    $('.vid-item-2').click(function() {
-        var embedUrl = $('.vid-item-2').data('embed_url');
-        // console.log(embedUrl);
-        // console.log("What's in iframe now: " + document.getElementById('vid_frame').src);
-        document.getElementById('vid_frame').src = "//" + embedUrl;
-        // console.log("Replaced: " + document.getElementById('vid_frame').src);
-    });
-    $('.vid-item-3').click(function() {
-        var embedUrl = $('.vid-item-3').data('embed_url');
+    $('.vid-item').click(function() {
+        var embedUrl = $(this).data('embed_url');
         // console.log(embedUrl);
         // console.log("What's in iframe now: " + document.getElementById('vid_frame').src);
         document.getElementById('vid_frame').src = "//" + embedUrl;
