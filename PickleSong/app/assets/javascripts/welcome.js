4992902bc9b2844bb6aa66520d1da4e66dbc0e22
Sam Blake <samuelmorganblake@gmail.com>
2014-07-23 19:57:21 -0400

    added scroll bar to results

--- PickleSong/app/assets/javascripts/welcome.js
+++ PickleSong/app/assets/javascripts/welcome.js
@@ -9,7 +9,7 @@ $(document).on('page:change', function() {
             $('#playlist_songs').append(template({
                 item: songData[i]
             }));
-        };
+        }
     });
 });
 
@@ -17,5 +17,20 @@ $(document).ready(function() {
     $('.vid-item').click(function() {
         var embedUrl = $(this).data('embed_url');
         document.getElementById('vid_frame').src = "//" + embedUrl;
+    });
+});
+
+$(document).ready(function () {
+    $(".arrow-right").bind("click", function (event) {
+        event.preventDefault();
+        $(".vid-list-container").stop().animate({
+            scrollLeft: "+=336"
+        }, 750);
+    });
+    $(".arrow-left").bind("click", function (event) {
+        event.preventDefault();
+        $(".vid-list-container").stop().animate({
+            scrollLeft: "-=336"
+        }, 750);
     });
 });
