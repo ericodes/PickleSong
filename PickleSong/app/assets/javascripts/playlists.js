e9a84fed92e4b847667345f8288f47815241379d
Janel diBiccari <janel.dibiccari@gmail.com>
2014-07-24 17:34:52 -0400

    add playlist title

--- PickleSong/app/assets/javascripts/playlists.js
+++ PickleSong/app/assets/javascripts/playlists.js
@@ -1,13 +1,12 @@
 $(document).on('page:change', function() {
-    $('.dropdown-toggle').click(function() {
-        // $(this).('.dropdown-menu');
-    });
     $('.playlist').click(function() {
         var songData = $(this).data('songs');
-        var playlistName = $(this).data('name');
         $('#playlist_songs').empty();
+        var playlistName = $(this).contents();
+        $('#playlist_songs').append('< h4 >'
+            playlistName '< /h4>');
         for (var i = 0; i < songData.length; i++) {
-            var templateString = "<div class='song'><h4><%= item.title %></h4><div><img src='<%=item.thumbnail%>'></div></div>";
+            var templateString = "<div class='song'><h4><%= item.title %></h4 > < div > < img src = '<%=item.thumbnail%>' > < /div></div > ";
             var template = _.template(templateString);
             $('#playlist_songs').append(template({
                 item: songData[i]
