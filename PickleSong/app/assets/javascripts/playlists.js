1721bae6d2dcc8ffa3610a12c155b16d816ffd2f
Janel diBiccari <janel.dibiccari@gmail.com>
2014-07-24 18:57:46 -0400

    sort of can delete

--- PickleSong/app/assets/javascripts/playlists.js
+++ PickleSong/app/assets/javascripts/playlists.js
@@ -2,11 +2,14 @@ $(document).on('page:change', function() {
     $('.playlist').click(function() {
         var songData = $(this).data('songs');
         $('#playlist_songs').empty();
-        var playlistName = $(this).contents();
-        $('#playlist_songs').append('< h4 >'
-            playlistName '< /h4>');
+        var playlist = $(this);
+        var titleTemplate = "<h4 class='<%=item.data('playlistid')%>'><%=item.data('name')%></h4>";
+        var titleTemp = _.template(titleTemplate);
+        $('#playlist_songs').append(titleTemp({
+            item: playlist
+        }));
         for (var i = 0; i < songData.length; i++) {
-            var templateString = "<div class='song'><h4><%= item.title %></h4 > < div > < img src = '<%=item.thumbnail%>' > < /div></div > ";
+            var templateString = "<div class='song'><h4><%= item.title %></h4><div><img src='<%=item.thumbnail%>'></div></div>";
             var template = _.template(templateString);
             $('#playlist_songs').append(template({
                 item: songData[i]
